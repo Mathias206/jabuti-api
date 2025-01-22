7bcf09b1c96466075b676fbfe6c07d447964b31c
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-01-22 19:19:50 -0300

    First commit

---
+++ src/main/java/com/mathias/jabuti/JabutiApiApplication.java
@@ -0,0 +1,13 @@
+package com.mathias.jabuti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabutiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JabutiApiApplication.class, args);
+	}
+
+}

59befe774010ec2487f2a10465fafa4aa534543c
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-03-08 16:29:21 -0300

    Enable custom repository

--- src/main/java/com/mathias/jabuti/JabutiApiApplication.java
+++ src/main/java/com/mathias/jabuti/JabutiApiApplication.java
@@ -2,12 +2,11 @@ package com.mathias.jabuti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.mathias.jabuti.infrastructure.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class JabutiApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JabutiApiApplication.class, args);
-	}
-
-}
+ public static void main(String[] args) { SpringApplication.run(JabutiApiApplication.class, args); } }

fc38382017d89c66698402883697b990c5cade26
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-03-20 16:22:03 -0300

    Add bean validation to goal and user models

--- src/main/java/com/mathias/jabuti/domain/model/Goal.java
+++ src/main/java/com/mathias/jabuti/domain/model/Goal.java
@@ -10,11 +10,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.ConvertGroup;
+import jakarta.validation.groups.Default;
+
 import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.mathias.jabuti.Groups.UserId;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
@@ -27,16 +35,21 @@ public class Goal {
   @Id
   private Long id;
 
+  @ConvertGroup(from = Default.class, to = UserId.class)
+  @Valid
+  @NotNull
   @ManyToOne
   @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
+  @NotBlank
   @Column(nullable = false)
   private String title;
 
   @Column(name = "goal_description")
   private String description;
 
+  @NotBlank
   @Enumerated(EnumType.STRING)
   private GoalType goalType;
 
--- src/main/java/com/mathias/jabuti/domain/model/User.java
+++ src/main/java/com/mathias/jabuti/domain/model/User.java
@@ -1,7 +1,12 @@
 package com.mathias.jabuti.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mathias.jabuti.Groups;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
@@ -13,14 +18,17 @@ import lombok.EqualsAndHashCode;
 @Table(name = "users")
 public class User {
 
+  @NotNull(groups = Groups.UserId.class)
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Id
   @EqualsAndHashCode.Include
   private Long id;
 
+  @NotBlank
   @Column(name = "user_name", nullable = false)
   private String name;
 
+  @NotBlank
   @Column(nullable = false)
   private String email;
 

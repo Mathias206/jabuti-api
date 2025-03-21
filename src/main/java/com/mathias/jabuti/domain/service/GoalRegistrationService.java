f01cfafb861d146c6526e778c757968c193177e0
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-03-21 19:48:25 -0300

    Add delete method

--- src/main/java/com/mathias/jabuti/domain/service/GoalRegistrationService.java
+++ src/main/java/com/mathias/jabuti/domain/service/GoalRegistrationService.java
@@ -11,8 +11,10 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,45 +22,53 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class GoalRegistrationService {
 
-	@Autowired
-	private GoalRepository goalRepository;
+    @Autowired
+    private GoalRepository goalRepository;
 
-	@Autowired
-	private EntityManager manager;
+    @Autowired
+    private EntityManager manager;
 
-	@Autowired
-	private UserRegistrationService userService;
+    @Autowired
+    private UserRegistrationService userService;
 
-	@Transactional
-	public Goal save(Goal goal) {
-		Long userId = goal.getUser().getId();
-		User user = userService.findOrFail(userId);
-		goal.setUser(user);
-		return goalRepository.save(goal);
-	}
+    @Transactional
+    public Goal save(Goal goal) {
+        Long userId = goal.getUser().getId();
+        User user = userService.findOrFail(userId);
+        goal.setUser(user);
+        return goalRepository.save(goal);
+    }
 
-	@Transactional
-	public Goal create(Goal goal) {
-		if (goal.getId() != null && goalRepository.existsById(goal.getId())) {
-			throw new DuplicatedEntityException(Goal.class, goal.getId());
-		}
-		Long userId = goal.getUser().getId();
-		User user = userService.findOrFail(userId);
-		goal.setUser(user);
-		return goalRepository.save(goal);
-	}
+    @Transactional
+    public Goal create(Goal goal) {
+        if (goal.getId() != null && goalRepository.existsById(goal.getId())) {
+            throw new DuplicatedEntityException(Goal.class, goal.getId());
+        }
+        Long userId = goal.getUser().getId();
+        User user = userService.findOrFail(userId);
+        goal.setUser(user);
+        return goalRepository.save(goal);
+    }
 
-	public List<Goal> criteriaCustomQuery(String goalName) {
-		CriteriaBuilder builder = manager.getCriteriaBuilder();
-		CriteriaQuery<Goal> criteria = builder.createQuery(Goal.class);
-		Root<Goal> root = criteria.from(Goal.class);
-		Predicate goalNamePredicate = builder.like(root.get("name"), "%" + goalName + "%");
-		criteria.where(goalNamePredicate);
-		TypedQuery<Goal> query = manager.createQuery(criteria);
-		return query.getResultList();
-	}
+    public List<Goal> criteriaCustomQuery(String goalName) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Goal> criteria = builder.createQuery(Goal.class);
+        Root<Goal> root = criteria.from(Goal.class);
+        Predicate goalNamePredicate = builder.like(root.get("name"), "%" + goalName + "%");
+        criteria.where(goalNamePredicate);
+        TypedQuery<Goal> query = manager.createQuery(criteria);
+        return query.getResultList();
+    }
 
-	public Goal findOrFail(Long goalId) {
-		return goalRepository.findById(goalId).orElseThrow(() -> new EntityNotFoundException(Goal.class, goalId));
-	}
+    public Goal findOrFail(Long goalId) {
+        return goalRepository.findById(goalId).orElseThrow(() -> new EntityNotFoundException(Goal.class, goalId));
+    }
+
+    @Transactional
+    public void delete(Long goalId) {
+        if (!goalRepository.existsById(goalId)) {
+            throw new EntityNotFoundException(Goal.class, goalId);
+        }
+        goalRepository.deleteById(goalId);
+    }
 }

4928fbe0fc0f5a5653898c7b7ab80ce645f7a316
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-03-21 10:14:39 -0300

    Insert data using utc

--- src/main/resources/seeds/user.sql
+++ src/main/resources/seeds/user.sql
@@ -1,16 +1,16 @@
 INSERT INTO
-  users (id, user_name, email)
+  users (id, user_name, email, updated_at, created_at)
 VALUES
-  (1, 'Alice Johnson', 'alice@example.com'),
-  (2, 'Bob Smith', 'bob@example.com'),
-  (3, 'Charlie Brown', 'charlie@example.com'),
-  (4, 'Diana Prince', 'diana@example.com'),
-  (5, 'Ethan Hunt', 'ethan@example.com'),
-  (6, 'Fiona Gallagher', 'fiona@example.com'),
-  (7, 'George Clooney', 'george@example.com'),
-  (8, 'Hannah Baker', 'hannah@example.com'),
-  (9, 'Ian Somerhalder', 'ian@example.com'),
-  (10, 'Jane Doe', 'jane@example.com')
+  (1, 'Alice Johnson', 'alice@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (2, 'Bob Smith', 'bob@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (3, 'Charlie Brown', 'charlie@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (4, 'Diana Prince', 'diana@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (5, 'Ethan Hunt', 'ethan@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (6, 'Fiona Gallagher', 'fiona@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (7, 'George Clooney', 'george@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (8, 'Hannah Baker', 'hannah@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (9, 'Ian Somerhalder', 'ian@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP),
+  (10, 'Jane Doe', 'jane@example.com', UTC_TIMESTAMP, UTC_TIMESTAMP)
 ON DUPLICATE KEY UPDATE
   user_name = VALUES(user_name),
   email = VALUES(email);

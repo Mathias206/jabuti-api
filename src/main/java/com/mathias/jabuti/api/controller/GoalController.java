ad4d44eb81c524bd1d207734d06aebf2ea2d474c
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-03-21 19:48:55 -0300

    Uses dto to handle controller data

--- src/main/java/com/mathias/jabuti/api/controller/GoalController.java
+++ src/main/java/com/mathias/jabuti/api/controller/GoalController.java
@@ -1,9 +1,14 @@
 package com.mathias.jabuti.api.controller;
 
+import com.mathias.jabuti.api.assembler.GoalDTOAssembler;
+import com.mathias.jabuti.api.assembler.GoalDTODisassembler;
+import com.mathias.jabuti.api.model.GoalDTO;
+import com.mathias.jabuti.api.model.input.GoalInputDTO;
 import com.mathias.jabuti.domain.exception.EntityNotFoundException;
 import com.mathias.jabuti.domain.model.Goal;
 import com.mathias.jabuti.domain.model.GoalPriority;
 import com.mathias.jabuti.domain.model.GoalStatus;
+import com.mathias.jabuti.domain.model.User;
 import com.mathias.jabuti.domain.repository.GoalRepository;
 import com.mathias.jabuti.domain.service.DomainException;
 import com.mathias.jabuti.domain.service.GoalRegistrationService;
@@ -25,50 +30,67 @@ import static com.mathias.jabuti.infrastructure.repository.specs.GoalSpecs.*;
 @RequestMapping(value = "/goal")
 public class GoalController {
 
-  @Autowired
-  private GoalRepository repository;
+    @Autowired
+    private GoalRepository repository;
 
-  @Autowired
-  private GoalRegistrationService service;
+    @Autowired
+    private GoalRegistrationService service;
 
-  @GetMapping
-  public List<Goal> list() {
-    return repository.findAll();
-  }
+    @Autowired
+    private GoalDTOAssembler goalDTOAssembler;
 
-  @GetMapping("/{goalId}")
-  public Goal retrieve(@PathVariable("goalId") Long id) {
-    return service.findOrFail(id);
-  }
+    @Autowired
+    private GoalDTODisassembler goalDTODisassembler;
 
-  @PostMapping
-  public ResponseEntity<?> create(@RequestBody @Valid Goal requestGoal) {
-    try {
-      Goal createdGoal = service.create(requestGoal);
-      return ResponseEntity.status(HttpStatus.CREATED).body(createdGoal);
-    } catch (EntityNotFoundException e) {
-      throw new DomainException(e.getMessage());
+    @GetMapping
+    public List<GoalDTO> list() {
+        return goalDTOAssembler.toCollectionModel(repository.findAll());
     }
-  }
 
-  @GetMapping("/specifications")
-  public List<Goal> getWithSpecifications() {
-    return repository.findAll(goalStatus(GoalStatus.PENDING).and(goalPriority(GoalPriority.MEDIUM)));
-  }
-
-  @GetMapping("/customJpaRepository")
-  public Optional<Goal> getFirstGoal() {
-    return repository.findFirst();
-  }
-
-  @PutMapping("/{goalId}")
-  public Goal update(@PathVariable("goalId") Long goalId, @RequestBody Goal requestGoal) {
-    Goal goal = service.findOrFail(goalId);
-    BeanUtils.copyProperties(requestGoal, goal, "id");
-    try {
-      return service.save(goal);
-    } catch (EntityNotFoundException e) {
-      throw new DomainException(e.getMessage());
+    @GetMapping("/{goalId}")
+    public GoalDTO retrieve(@PathVariable("goalId") Long id) {
+        Goal goal = service.findOrFail(id);
+        return goalDTOAssembler.toModel(goal);
     }
-  }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody @Valid GoalInputDTO goalInput) {
+        try {
+            Goal goal = goalDTODisassembler.toDomainObject(goalInput);
+            GoalDTO createdGoal = goalDTOAssembler.toModel(service.create(goal));
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdGoal);
+        } catch (EntityNotFoundException e) {
+            throw new DomainException(e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/specifications")
+    public List<Goal> getWithSpecifications() {
+        return repository.findAll(goalStatus(GoalStatus.PENDING).and(goalPriority(GoalPriority.MEDIUM)));
+    }
+
+    @GetMapping("/customJpaRepository")
+    public Optional<Goal> getFirstGoal() {
+        return repository.findFirst();
+    }
+
+    @PutMapping("/{goalId}")
+    public GoalDTO update(@PathVariable("goalId") Long goalId, @RequestBody @Valid GoalInputDTO goalInputDTO) {
+        Goal goal = service.findOrFail(goalId);
+        goalDTODisassembler.copyToDomainObject(goalInputDTO, goal);
+        try {
+            var savedGoal = service.save(goal);
+            return goalDTOAssembler.toModel(savedGoal);
+        } catch (EntityNotFoundException e) {
+            throw new DomainException(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{goalId}")
+    public ResponseEntity<?> delete(@PathVariable("goalId") Long goalId) {
+        service.delete(goalId);
+
+        return ResponseEntity.noContent().build();
+    }
 }

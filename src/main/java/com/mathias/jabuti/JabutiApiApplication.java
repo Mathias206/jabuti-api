cf9323fd1063f51ee8b00bdfffcded6af22fd2fb
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-03-21 10:14:21 -0300

    Set project and db to work with UTC timezone

--- src/main/java/com/mathias/jabuti/JabutiApiApplication.java
+++ src/main/java/com/mathias/jabuti/JabutiApiApplication.java
@@ -1,5 +1,7 @@
 package com.mathias.jabuti;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,4 +11,8 @@ import com.mathias.jabuti.infrastructure.repository.CustomJpaRepositoryImpl;
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class JabutiApiApplication {
- public static void main(String[] args) { SpringApplication.run(JabutiApiApplication.class, args); } }
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(JabutiApiApplication.class, args);
+    }
+}

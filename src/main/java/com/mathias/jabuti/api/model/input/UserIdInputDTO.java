db22e58588475efb5c52f0c5fbe015171862cd52
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-04-03 18:50:15 -0300

    Create dto to track user id

---
+++ src/main/java/com/mathias/jabuti/api/model/input/UserIdInputDTO.java
@@ -0,0 +1,11 @@
+package com.mathias.jabuti.api.model.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+    public class UserIdInputDTO {
+
+        private Long id;
+    }

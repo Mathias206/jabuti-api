d06e6d44824d037588ccfa6b1fcc927a132bbec7
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2025-05-17 19:54:39 -0300

    Add methodo to only get user specific fields and improve findByEmail qury

--- src/main/java/com/mathias/jabuti/domain/repository/UserRepository.java
+++ src/main/java/com/mathias/jabuti/domain/repository/UserRepository.java
@@ -1,12 +1,22 @@
 package com.mathias.jabuti.domain.repository;
 
+import com.mathias.jabuti.api.model.UserEmailPasswordProjection;
 import com.mathias.jabuti.domain.model.User;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email); 
+    @EntityGraph(attributePaths = {"groups", "groups.permissions"})
+    Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("select u.email as email, u.password as password from User u where u.email = :email")
+    Optional<UserEmailPasswordProjection> findByEmailProjected(@Param("email") String email);
+
+
 }
